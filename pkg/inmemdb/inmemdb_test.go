385cd94adf70cda24e137385fe1cff13369ec3a7
Daniel Cibrão <72982734+gyuzeh@users.noreply.github.com>
2020-11-28 23:34:45 +0000

    [FEAT] In Memory Db Logic and Unit Tests (#4)

---
+++ pkg/inmemdb/inmemdb_test.go
@@ -0,0 +1,98 @@
+package inmemdb
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Employee struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	City      string `json:"city"`
+}
+
+func TestSetWhenKeyAndStringValueAreProvidedThenIsSaved(t *testing.T) {
+	// Arrange
+	key := "12345"
+	data := `
+    {
+        "firstname": "Rocky",
+        "lastname": "Sting",
+        "city": "London"
+    }`
+	memdb := NewMemDb()
+
+	// Act
+	result, err := memdb.Set(key, data)
+
+	// Assert
+	assert.Nil(t, err)
+	assert.True(t, result)
+}
+
+func TestSetWhenKeyAndObjectValueAreProvidedThenIsSaved(t *testing.T) {
+	// Arrange
+	key := "empKey"
+	data := Employee{FirstName: "Rocky", LastName: "Sting", City: "London"}
+	memdb := NewMemDb()
+
+	// Act
+	result, err := memdb.Set(key, data)
+
+	// Assert
+	assert.Nil(t, err)
+	assert.True(t, result)
+}
+
+func TestSetWhenValueWithExistingKeyIsProvidedThenItShouldFail(t *testing.T) {
+	// Arrange
+	key := "12345"
+	data := Employee{FirstName: "Rocky", LastName: "Sting", City: "London"}
+	memdb := NewMemDb()
+	memdb.Set(key, data)
+
+	// Act
+	result, err := memdb.Set(key, data)
+
+	// Assert
+	assert.NotNil(t, err)
+	assert.Equal(t, "Duplicated Key 12345", err.Error())
+	assert.False(t, result)
+}
+
+func TestGetWhenKeyExitsThenReturnValue(t *testing.T) {
+	// Arrange
+	key := "key"
+	data := Employee{FirstName: "Rocky", LastName: "Sting", City: "London"}
+	memdb := NewMemDb()
+	memdb.Set(key, data)
+
+	// Act
+	result := memdb.Get(key)
+
+	// Assert
+	assert.Equal(t, data, result)
+}
+
+func TestGetWhenKeyExitsThenDelete(t *testing.T) {
+	// Arrange
+	key := "key"
+	memdb := NewMemDb()
+	memdb.Set("qwerty", "random value")
+
+	// Act
+	memdb.Delete(key)
+
+	// Assert
+	assert.Nil(t, memdb.Get(key))
+}
+
+func TestDeleteWhenKeyDoesNotExitsThenDontThrowError(t *testing.T) {
+	// Arrange
+	key := "unExistingKey"
+	memdb := NewMemDb()
+
+	// Act & Assert
+	memdb.Delete(key)
+}
---
+++ pkg/inmemdb/inmemdn.go
@@ -0,0 +1,45 @@
+package inmemdb
+
+// DbOperations that can be executed in the database
+type DbOperations interface {
+	Set(key string, data interface{})
+	Get(key string)
+	Delete(key string)
+}
+
+// MemDb is a Volatile Memory Database
+type MemDb struct {
+	Db map[string]interface{}
+}
+
+// NewMemDb Creates a new instance of MemDb
+func NewMemDb() *MemDb {
+	memDb := MemDb{Db: make(map[string]interface{})}
+	return &memDb
+}
+
+// Set an Object in the MemDb
+func (mdb *MemDb) Set(key string, data interface{}) (bool, error) {
+
+	if _, ok := mdb.Db[key]; ok {
+		return false, DuplicateKeyError{key: key}
+	}
+
+	mdb.Db[key] = data
+
+	return true, nil
+}
+
+// Get an Object in the MemDb
+func (mdb *MemDb) Get(key string) interface{} {
+	return mdb.Db[key]
+}
+
+// Delete an Object in the MemDb
+func (mdb *MemDb) Delete(key string) {
+	_, ok := mdb.Db[key]
+
+	if ok {
+		delete(mdb.Db, "moo")
+	}
+}
